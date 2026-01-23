562cb6d772c0a35e8c22581c50aa1faf4f903828
dongri-p <hdb151@naver.com>
2026-01-24 01:21:52 +0900

    기본설정2

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
@@ -7,7 +8,13 @@ import New from "./pages/New";
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary" element={<Diary />} />
+    </Routes>
+  );
 }
 
 export default App;
