06c390429ebcd9ecf1241c32dc122e6fa1407300
dongri-p <hdb151@naver.com>
2026-01-28 17:56:11 +0900

    button

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/NotFound";
+import Button from "./components/Button";
 
 import { getEmotionImage } from "./util/get-emotion-image";
 
@@ -17,19 +18,7 @@ function App() {
   };
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Button />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
