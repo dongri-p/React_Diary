27aafbf77f19e12361cb53a3ac8127e4b6942a28
dongri-p <hdb151@naver.com>
2026-01-28 19:23:48 +0900

    app 정리

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,59 +1,28 @@
 import "./App.css";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { useReducer } from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Notfound from "./pages/NotFound";
 
-import Button from "./components/Button";
-import Header from "./components/Header";
-
-import { getEmotionImage } from "./util/get-emotion-image";
-
 // 1."/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+function reducer(state, action) {
+  return state;
+}
+
 function App() {
-  const nav = useNavigate();
-  const onClickButton = () => {
-    nav("/new");
-  };
+  const [data, dispatch] = useReducer(reducer, []);
   return (
     <>
-      <Header
-        title={"Header"}
-        leftChild={<Button text={"Left"} />}
-        rightChild={<Button text={"right"} />}
-      />
-      <Button
-        text={"123"}
-        // type={"DEFAULT"} -> default 는 생략가능
-        onClick={() => {
-          console.log("123번 클릭");
-        }}
-      />
-
-      <Button
-        text={"123"}
-        type={"POSITIVE"}
-        onClick={() => {
-          console.log("123번 클릭");
-        }}
-      />
-
-      <Button
-        text={"123"}
-        type={"NEGATIVE"}
-        onClick={() => {
-          console.log("123번 클릭");
-        }}
-      />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -1,5 +1,9 @@
+import { useParams } from "react-router-dom";
+
 const Edit = () => {
-  return <div>Edit</div>;
+  const params = useParams();
+
+  return <div>{params.id}번째 일기</div>;
 };
 
 export default Edit;
