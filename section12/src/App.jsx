774cf2654d404d46f599b4b69aa5a5ed016aa37b
dongri-p <hdb151@naver.com>
2026-01-30 12:19:02 +0900

    delete

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -33,6 +33,10 @@ function reducer(state, action) {
       return state.map((item) =>
         String(item.id) === String(action.data.id) ? action.data : item,
       );
+    case "DELETE":
+      return state.filter((item) => String(item.id) !== String(action.id));
+    default:
+      return state;
   }
 }
 
@@ -64,6 +68,13 @@ function App() {
     });
   };
 
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      id,
+    });
+  };
+
   return (
     <>
       <button
@@ -80,6 +91,14 @@ function App() {
         }}
       >
         일기 수정 테스트
+      </button>
+
+      <button
+        onClick={() => {
+          onDelete(1);
+        }}
+      >
+        일기 삭제 테스트
       </button>
       <Routes>
         <Route path="/" element={<Home />} />
