79fcd51fd623209f2d5e8550c68ea768d833f230
dongri-p <hdb151@naver.com>
2026-01-30 13:12:40 +0900

    diaryitem css

--- section12/src/components/DiaryItem.jsx
+++ section12/src/components/DiaryItem.jsx
@@ -1,11 +1,13 @@
 import { getEmotionImage } from "../util/get-emotion-image";
 import Button from "./Button";
+import "./DiaryItem.css";
 
 const DiaryItem = () => {
+  const emotionId = 1;
   return (
     <div className="DiaryItem">
-      <div className="img_section">
-        <img src={getEmotionImage(1)} />
+      <div className={`img_section img_section_${emotionId}`}>
+        <img src={getEmotionImage(emotionId)} />
       </div>
       <div className="info_section">
         <div className="created_date">{new Date().toLocaleDateString()}</div>
