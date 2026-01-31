de9bb25d86e4a46ed0509afaf1936392510c5f61
dongri-p <hdb151@naver.com>
2026-01-31 15:39:16 +0900

    new onsubmit button

--- section12/src/components/Editor.jsx
+++ section12/src/components/Editor.jsx
@@ -42,7 +42,7 @@ const getStringedDate = (targetDate) => {
   return `${year}-${month}-${date}`;
 };
 
-const Editor = () => {
+const Editor = ({ onSubmit }) => {
   const [input, setInput] = useState({
     createdDate: new Date(),
     emotionId: 3,
@@ -61,6 +61,10 @@ const Editor = () => {
       ...input,
       [name]: value,
     });
+  };
+
+  const onClickSubmitButton = () => {
+    onSubmit(input);
   };
 
   return (
@@ -105,7 +109,11 @@ const Editor = () => {
       </section>
       <section className="button_section">
         <Button text={"취소하기"} />
-        <Button text={"작성완료"} type={"POSITIVE"} />
+        <Button
+          onClick={onClickSubmitButton}
+          text={"작성완료"}
+          type={"POSITIVE"}
+        />
       </section>
     </div>
   );
