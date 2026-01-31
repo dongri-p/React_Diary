3cbf6ebb213465e17900dfe4acf42fb29ac18cd6
dongri-p <hdb151@naver.com>
2026-01-31 13:57:44 +0900

    editor emotion_section

--- section12/src/components/Editor.jsx
+++ section12/src/components/Editor.jsx
@@ -1,6 +1,32 @@
 import "./Editor.css";
+import EmotionItem from "./EmotionItem";
+
+const emotionList = [
+  {
+    emotionId: 1,
+    emotionName: "완전 좋음",
+  },
+  {
+    emotionId: 2,
+    emotionName: "좋음",
+  },
+  {
+    emotionId: 3,
+    emotionName: "그럭 저럭",
+  },
+  {
+    emotionId: 4,
+    emotionName: "나쁨",
+  },
+  {
+    emotionId: 5,
+    emotionName: "끔찍함",
+  },
+];
 
 const Editor = () => {
+  const emotionId = 5;
+
   return (
     <div className="Editor">
       <section className="date_section">
@@ -9,6 +35,15 @@ const Editor = () => {
       </section>
       <section className="emotion_section">
         <h4>오늘의 감정</h4>
+        <div className="emotion_list_wrapper">
+          {emotionList.map((item) => (
+            <EmotionItem
+              key={item.emotionId}
+              {...item}
+              isSelected={item.emotionId === emotionId}
+            />
+          ))}
+        </div>
       </section>
       <section className="content_section"></section>
       <section className="button_section"></section>
