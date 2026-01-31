67a3ac366a4f04a2954538bf5438ff27fc30b6f7
dongri-p <hdb151@naver.com>
2026-01-31 13:14:13 +0900

    new page 2(emotionitem)

--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import Button from "../components/Button";
+import Editor from "../components/Editor";
 
 const New = () => {
   return (
@@ -8,6 +9,7 @@ const New = () => {
         title={"새 일기 쓰기"}
         leftChild={<Button text={"< 뒤로 가기"} />}
       />
+      <Editor />
     </div>
   );
 };
