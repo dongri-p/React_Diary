73de9f0e1b3f662b7a650abef2c42205f5d556d4
dongri-p <hdb151@naver.com>
2026-02-02 14:47:24 +0900

    edit 수정완료

--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -8,7 +8,7 @@ import { DiaryDispatchContext, DiaryStateContext } from "../App";
 const Edit = () => {
   const params = useParams();
   const nav = useNavigate();
-  const { onDelete } = useContext(DiaryDispatchContext);
+  const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
   const data = useContext(DiaryStateContext);
   const [curDiaryItem, setCurDiaryItem] = useState();
 
@@ -33,6 +33,18 @@ const Edit = () => {
     }
   };
 
+  const onSubmit = (input) => {
+    if (window.confirm("일기를 수정하시겠습니까?")) {
+      onUpdate(
+        params.id,
+        input.createdDate.getTime(),
+        input.emotionId,
+        input.content,
+      );
+      nav("/", { replace: true });
+    }
+  };
+
   return (
     <div>
       <Header
@@ -42,7 +54,7 @@ const Edit = () => {
           <Button onClick={onClickDelete} text={"삭제하기"} type={"NEGATIVE"} />
         }
       />
-      <Editor initData={curDiaryItem} />
+      <Editor initData={curDiaryItem} onSubmit={onSubmit} />
     </div>
   );
 };
