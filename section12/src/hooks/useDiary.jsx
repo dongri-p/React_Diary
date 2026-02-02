2e21a4f6cd9a74f216ffa8f599f45791d69a3f9e
dongri-p <hdb151@naver.com>
2026-02-02 16:25:59 +0900

    diary

--- section12/src/hooks/useDiary.jsx
+++ section12/src/hooks/useDiary.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState, useEffect } from "react";
-import { DiaryStateContext } from ".../App";
+import { DiaryStateContext } from "../App";
 import { useNavigate } from "react-router-dom";
 
 const useDiary = (id) => {
@@ -8,7 +8,9 @@ const useDiary = (id) => {
   const nav = useNavigate();
 
   useEffect(() => {
-    const currentDiaryItem = data.find((item) => String(id) === String(id));
+    const currentDiaryItem = data.find(
+      (item) => String(item.id) === String(id),
+    );
 
     if (!currentDiaryItem) {
       window.alert("존재하지 않는 일기입니다.");
