d1a8c251b6d76af6812f11dbd05929fab5a95cfc
dongri-p <hdb151@naver.com>
2026-02-03 11:54:37 +0900

    diary 완료

--- section12/src/pages/Diary.jsx
+++ section12/src/pages/Diary.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Button from "../components/Button";
 import Viewer from "../components/Viewer";
 import useDiary from "../hooks/useDiary";
+import { getStringedDate } from "../util/get-stringed-date";
 
 const Diary = () => {
   const params = useParams();
@@ -14,12 +15,13 @@ const Diary = () => {
     return <div>데이터 로딩중...</div>;
   }
 
-  const [createdDate, emotionId, content] = curDiaryItem;
+  const { createdDate, emotionId, content } = curDiaryItem;
+  const title = getStringedDate(new Date(createdDate));
 
   return (
     <div>
       <Header
-        title={"yyyy-mm-dd 기록"}
+        title={`${title} 기록`}
         leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로가기"} />}
         rightChild={
           <Button onClick={() => nav(`/edit/${params.id}`)} text={"수정하기"} />
